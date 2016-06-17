f5b86ee5d58fa801c5fc5fe4fb59934113218695
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2016-06-17 19:06:57 +0200

    Some test and modifications as the test complains

---
+++ test/t1.zsh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+emulate -LR zsh
+source ~/.zekyll/zekyll/lib/math_functions
+source lib/math_functions ; encode_zcode_arr01 1 1 1 1 0 1 0 1 0 1 0 1 0 1 0 1 1 1 1 1 1 0 0 0 1 0 0 1 0 1 ; echo $reply ; echo $REPLY
